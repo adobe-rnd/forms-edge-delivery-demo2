72e0edae4918876fd7f109d66fa857c1dc16c150
Vaibhav Varshney <vaibhav1313@gmail.com>
2024-07-10 14:57:48 +0530

    Update worker.js

--- blocks/form/rules/worker.js
+++ blocks/form/rules/worker.js
@@ -24,7 +24,7 @@ export default async function initializeRuleEngineWorker(formDef, renderHTMLForm
     return renderHTMLForm(form.getState(true), formDef.data);
   }
   const myWorker = new Worker(`${window.hlx.codeBasePath}/blocks/form/rules/RuleEngineWorker.js`, { type: 'module' });
- formDef.windowObject = window;
+ formDef.windowSearch = window.location.search;
   myWorker.postMessage({
     name: 'init',
     payload: formDef,
@@ -35,7 +35,7 @@ export default async function initializeRuleEngineWorker(formDef, renderHTMLForm
     myWorker.addEventListener('message', async (e) => {
       if (e.data.name === 'init') {
         form = await renderHTMLForm(e.data.payload);
-        windowObject=e.data.payload.windowObject;
+        windowSearch=e.data.payload.windowSearch;
         // myWorker.terminate();
         resolve(form);
       }
