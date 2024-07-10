f0b0c2780899936a29eec823efb80291c4ce3b03
Vaibhav Varshney <vaibhav1313@gmail.com>
2024-07-10 14:58:29 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -43,7 +43,7 @@ function generateUniqueID () {
  * @return {string}
  */
 function getURL_Parameter () {
-    var queryString = windowObject.location.search;
+    var queryString = windowSearch;
     var urlParams = new URLSearchParams(queryString);
      if(urlParams.has('generatedID')){
          var generatedID = urlParams.get('generatedID');
